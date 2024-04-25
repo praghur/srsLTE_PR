589e2e446b110abffb27a373a142924605371b1b
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 16:40:36 -0400

    Delete start-enb2.sh

--- start-enb2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ux
-tmux new-session -d -s enb
-tmux send-keys 'sudo srsenb /local/repository/etc/enb2.conf' C-m
-tmux split-window -v
-tmux select-layout even-vertical
-tmux attach-session -d -t enb

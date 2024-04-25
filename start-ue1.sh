e7db5485b0850c7b6ed7eaf310fa1584f7d70792
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 15:56:12 -0400

    Update and rename start-ue.sh to start-ue1.sh

--- start-ue1.sh
+++ start-ue1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ux
 tmux new-session -d -s ue
-tmux send-keys 'sudo srsue /local/repository/etc/ue.conf' C-m
+tmux send-keys 'sudo srsue /local/repository/etc/ue1.conf' C-m
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t ue

d81e4b6b78f2908a423216e7c457ffc9a0e56184
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 16:04:38 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -89,11 +89,11 @@ iface2 = enb1.addInterface("eth1")
 iface2.addAddress(rspec.IPv4Address("10.10.1.2", "255.255.255.0"))
 
 # Add eNB2 node
-enb2 = request.RawPC("enb2")
-enb2.hardware_type = GLOBALS.HWTYPE
-enb2.disk_image = GLOBALS.SRSLTE_IMG
-iface4 = enb2.addInterface("eth1")
-iface4.addAddress(rspec.IPv4Address("10.10.1.3", "255.255.255.0"))
+#enb2 = request.RawPC("enb2")
+#enb2.hardware_type = GLOBALS.HWTYPE
+#enb2.disk_image = GLOBALS.SRSLTE_IMG
+#iface4 = enb2.addInterface("eth1")
+#iface4.addAddress(rspec.IPv4Address("10.10.1.3", "255.255.255.0"))
 
 # Add LL UE1 node
 rue1 = request.RawPC("rue1")
@@ -103,18 +103,18 @@ iface3 = rue1.addInterface("eth1")
 iface3.addAddress(rspec.IPv4Address("10.10.1.4", "255.255.255.0"))
 
 # Add LL UE2 node
-rue2 = request.RawPC("rue2")
-rue2.hardware_type = GLOBALS.HWTYPE
-rue2.disk_image = GLOBALS.SRSLTE_IMG
-iface5 = rue2.addInterface("eth1")
-iface5.addAddress(rspec.IPv4Address("10.10.1.5", "255.255.255.0"))
+#rue2 = request.RawPC("rue2")
+#rue2.hardware_type = GLOBALS.HWTYPE
+#rue2.disk_image = GLOBALS.SRSLTE_IMG
+#iface5 = rue2.addInterface("eth1")
+#iface5.addAddress(rspec.IPv4Address("10.10.1.5", "255.255.255.0"))
 
 link = request.LAN("lan")
 link.addInterface(iface1)
 link.addInterface(iface2)
 link.addInterface(iface3)
-link.addInterface(iface4)
-link.addInterface(iface5)
+#link.addInterface(iface4)
+#link.addInterface(iface5)
 
 link.link_multiplexing = True
 link.vlan_tagging = True

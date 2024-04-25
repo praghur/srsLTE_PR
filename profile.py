87420f07c3d616e28f21d3f299fa790217e6ad52
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 15:55:15 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -113,8 +113,8 @@ link = request.LAN("lan")
 link.addInterface(iface1)
 link.addInterface(iface2)
 link.addInterface(iface3)
-link1.addInterface(iface4)
-link1.addInterface(iface5)
+link.addInterface(iface4)
+link.addInterface(iface5)
 
 link.link_multiplexing = True
 link.vlan_tagging = True

372899c960bc7b8c318bdca23bb0466ab885622c
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 15:48:48 -0400

    Update and rename start-enb.sh to start-enb1.sh

--- start-enb1.sh
+++ start-enb1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ux
 tmux new-session -d -s enb
-tmux send-keys 'sudo srsenb /local/repository/etc/enb.conf' C-m
+tmux send-keys 'sudo srsenb /local/repository/etc/enb1.conf' C-m
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t enb

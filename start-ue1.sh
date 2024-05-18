193d9591a074481375580898c7bc9f81de6371e7
praghur <166060957+praghur@users.noreply.github.com>
2024-05-18 18:29:47 -0400

    Update start-ue1.sh

--- start-ue1.sh
+++ start-ue1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ux
-tmux new-session -d -s ue
+tmux new-session -d -s ue1
 tmux send-keys 'sudo srsue /local/repository/etc/ue1.conf' C-m
 tmux split-window -v
 tmux select-layout even-vertical
-tmux attach-session -d -t ue
+tmux attach-session -d -t ue1

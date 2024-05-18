0d59ca0bcca1a03f0c40a49b8eb65e9fbb25d54c
praghur <166060957+praghur@users.noreply.github.com>
2024-05-18 18:29:59 -0400

    Update start-ue2.sh

--- start-ue2.sh
+++ start-ue2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ux
-tmux new-session -d -s ue
+tmux new-session -d -s ue2
 tmux send-keys 'sudo srsue /local/repository/etc/ue2.conf' C-m
 tmux split-window -v
 tmux select-layout even-vertical
-tmux attach-session -d -t ue
+tmux attach-session -d -t ue2

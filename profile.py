5fe67de1573b14b8a562eedfdc098158aa9db92d
praghur <166060957+praghur@users.noreply.github.com>
2024-05-18 18:43:03 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -110,11 +110,11 @@ iface5 = rue2.addInterface("eth1")
 iface5.addAddress(rspec.IPv4Address("10.10.1.5", "255.255.255.0"))
 
 # Add eNB3_Remote node
-enb3r = request.RawPC("enb3r")
-enb3r.hardware_type = GLOBALS.HWTYPE
-enb3r.disk_image = GLOBALS.SRSLTE_IMG
-iface6 = enb3r.addInterface("eth2")
-iface6.addAddress(rspec.IPv4Address("172.168.1.2", "255.255.255.0"))
+#enb3r = request.RawPC("enb3r")
+#enb3r.hardware_type = GLOBALS.HWTYPE
+#enb3r.disk_image = GLOBALS.SRSLTE_IMG
+#iface6 = enb3r.addInterface("eth2")
+#iface6.addAddress(rspec.IPv4Address("172.168.1.2", "255.255.255.0"))
 
 link = request.LAN("lan")
 link.addInterface(iface1)
@@ -122,7 +122,7 @@ link.addInterface(iface2)
 link.addInterface(iface3)
 link.addInterface(iface4)
 link.addInterface(iface5)
-link.addInterface(iface6)
+#link.addInterface(iface6)
 
 link.link_multiplexing = True
 link.vlan_tagging = True

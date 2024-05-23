26101ca98abadcd5f4a3294f7583af5a3549070c
praghur <166060957+praghur@users.noreply.github.com>
2024-05-23 12:11:59 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ if [ $NODE_ID = "rue1" ]; then
     /local/repository/start-ue1.sh
 elif [ $NODE_ID = "rue2" ]; then
    /local/repository/start-ue2.sh
+elif [ $NODE_ID = "rue3" ]; then
+   /local/repository/start-ue3.sh
 elif [ $NODE_ID = "enb1" ]; then
     /local/repository/start-enb1.sh
 elif [ $NODE_ID = "enb2" ]; then

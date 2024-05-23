c1c1ff38541e284ab5229ba5dd5737683d23ed79
praghur <166060957+praghur@users.noreply.github.com>
2024-05-23 14:15:48 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -78,8 +78,8 @@ epc.hardware_type = GLOBALS.HWTYPE
 epc.disk_image = GLOBALS.SRSLTE_IMG
 iface1 = epc.addInterface("eth1")
 iface1.addAddress(rspec.IPv4Address("10.10.1.1", "255.255.255.0"))
-iface1a = epc.addInterface("eth2")
-iface1a.addAddress(rspec.IPv4Address("172.168.1.1", "255.255.255.0"))
+#iface1a = epc.addInterface("eth2")
+#iface1a.addAddress(rspec.IPv4Address("10.10.1.6", "255.255.255.0"))
 
 # Add eNB1 node
 enb1 = request.RawPC("enb1")
@@ -114,14 +114,14 @@ rue3 = request.RawPC("rue3")
 rue3.hardware_type = GLOBALS.HWTYPE
 rue3.disk_image = GLOBALS.SRSLTE_IMG
 iface7 = rue3.addInterface("eth1")
-iface7.addAddress(rspec.IPv4Address("172.168.1.3", "255.255.255.0"))
+iface7.addAddress(rspec.IPv4Address("10.10.1.7", "255.255.255.0"))
 
 # Add eNB3_Remote node
 enb3r = request.RawPC("enb3r")
 enb3r.hardware_type = GLOBALS.HWTYPE
 enb3r.disk_image = GLOBALS.SRSLTE_IMG
 iface6 = enb3r.addInterface("eth1")
-iface6.addAddress(rspec.IPv4Address("172.168.1.2", "255.255.255.0"))
+iface6.addAddress(rspec.IPv4Address("10.10.1.8", "255.255.255.0"))
 
 # Create two separate LAN links
 link1 = request.LAN("lan1")
@@ -134,7 +134,7 @@ link1.addInterface(iface3)
 link1.addInterface(iface4)
 link1.addInterface(iface5)
 
-link2.addInterface(iface1a)
+#link2.addInterface(iface1a)
 link2.addInterface(iface6)
 link2.addInterface(iface7)
 

6bc8c53859066a84b5386637f87c8bcaf9d233b6
praghur <166060957+praghur@users.noreply.github.com>
2024-05-23 14:51:38 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,18 +5,25 @@
 NODE_ID=$(geni-get client_id)
 
 if [ $NODE_ID = "rue1" ]; then
+    cdmod +x /local/repository/start-ue1.sh
     /local/repository/start-ue1.sh
 elif [ $NODE_ID = "rue2" ]; then
+    cdmod +x /local/repository/start-ue2.sh
    /local/repository/start-ue2.sh
 elif [ $NODE_ID = "rue3" ]; then
+    cdmod +x /local/repository/start-ue3.sh
    /local/repository/start-ue3.sh
 elif [ $NODE_ID = "enb1" ]; then
+    cdmod +x /local/repository/start-enb1.sh
     /local/repository/start-enb1.sh
 elif [ $NODE_ID = "enb2" ]; then
+    cdmod +x /local/repository/start-enb2.sh
     /local/repository/start-enb2.sh
 elif [ $NODE_ID = "enb3r" ]; then
+    cdmod +x /local/repository/start-enb3r.sh
     /local/repository/start-enb3r.sh
 elif [ $NODE_ID = "epc" ]; then
+    cdmod +x /local/repository/start-epc.sh
     /local/repository/start-epc.sh
 else
     echo "no setup necessary"

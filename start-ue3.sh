60395a1e10107c676fc9167a6f833d47eba7bb79
praghur <166060957+praghur@users.noreply.github.com>
2024-05-23 13:05:30 -0400

    Update start-ue3.sh

--- start-ue3.sh
+++ start-ue3.sh
@@ -4,4 +4,4 @@ tmux new-session -d -s ue3
 tmux send-keys 'sudo srsue /local/repository/etc/ue3.conf' C-m
 tmux split-window -v
 tmux select-layout even-vertical
-tmux attach-session -d -t ue2
+tmux attach-session -d -t ue3

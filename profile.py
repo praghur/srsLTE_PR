7ff00e29d4e35b7b86f1decd6f4dce7a256714f7
praghur <166060957+praghur@users.noreply.github.com>
2024-05-23 08:45:59 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -122,7 +122,7 @@ link.addInterface(iface2)
 link.addInterface(iface3)
 link.addInterface(iface4)
 link.addInterface(iface5)
-#link.addInterface(iface6)
+link.addInterface(iface6)
 
 link.link_multiplexing = True
 link.vlan_tagging = True

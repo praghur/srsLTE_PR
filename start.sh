4ca2157eeb2c019b3de83ef9cc9f2eb38ce58199
praghur <166060957+praghur@users.noreply.github.com>
2024-06-01 15:55:54 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -24,7 +24,6 @@ elif [ $NODE_ID = "enb3r" ]; then
     /local/repository/start-enb3r.sh
 elif [ $NODE_ID = "epc" ]; then
     sudo killall srsepc
-    chmod +x /etc/srslte/user_db.csv
     sudo echo "ue1,xor,001010123456789,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,000000001234,7,dynamic
     ue2,xor,001010123456790,00112233445566778899aabbccddeeee,opc,63bfa50ee6523365ff14c1f45f88737d,8000,000000001235,7,dynamic
     ue3,xor,001010123456791,00112233445566778899aabbccddffff,opc,63bfa50ee6523365ff14c1f45f88737d,7000,000000001236,7,dynamic" > /etc/srslte/user_db.csv

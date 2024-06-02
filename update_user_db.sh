e549c7b5c0b78ce0125f03551a477814c72ae10b
praghur <166060957+praghur@users.noreply.github.com>
2024-06-02 13:24:25 -0400

    Update update_user_db.sh

--- update_user_db.sh
+++ update_user_db.sh
@@ -9,6 +9,6 @@ ue2,xor,001010123456790,00112233445566778899aabbccddeeee,opc,63bfa50ee6523365ff1
 ue3,xor,001010123456791,00112233445566778899aabbccddffff,opc,63bfa50ee6523365ff14c1f45f88737d,7000,000000001236,7,dynamic"
 
 # Append the data to the user_db.csv file
-echo "$DATA" >> "$USER_DB_FILE"
+echo "$DATA" > "$USER_DB_FILE"
 
 echo "Data added successfully to $USER_DB_FILE"

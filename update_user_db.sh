eafc9cfcd3e0709e16874a25623ede7e8b3500c9
praghur <166060957+praghur@users.noreply.github.com>
2024-06-02 13:32:02 -0400

    Update update_user_db.sh

--- update_user_db.sh
+++ update_user_db.sh
@@ -8,7 +8,7 @@ DATA="ue1,xor,001010123456789,00112233445566778899aabbccddeeff,opc,63bfa50ee6523
 ue2,xor,001010123456790,00112233445566778899aabbccddeeee,opc,63bfa50ee6523365ff14c1f45f88737d,8000,000000001235,7,dynamic
 ue3,xor,001010123456791,00112233445566778899aabbccddffff,opc,63bfa50ee6523365ff14c1f45f88737d,7000,000000001236,7,dynamic"
 
-# Append the data to the user_db.csv file
+# Replace the data to the user_db.csv file
 echo "$DATA" > "$USER_DB_FILE"
 
 echo "Data added successfully to $USER_DB_FILE"

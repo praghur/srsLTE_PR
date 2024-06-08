d5d3bee5011e5fc025b21a948695faa8c1f4bc5b
praghur <166060957+praghur@users.noreply.github.com>
2024-06-08 11:24:56 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -82,14 +82,14 @@ iface1.addAddress(rspec.IPv4Address("10.10.1.1", "255.255.255.0"))
 #iface1a.addAddress(rspec.IPv4Address("10.10.1.6", "255.255.255.0"))
 
 # Add eNB1 node
-enb1 = request.RawPC("TS")
+enb1 = request.RawPC("enb1")
 enb1.hardware_type = GLOBALS.HWTYPE
 enb1.disk_image = GLOBALS.SRSLTE_IMG
 iface2 = enb1.addInterface("eth1")
 iface2.addAddress(rspec.IPv4Address("10.10.1.2", "255.255.255.0"))
 
 # Add eNB2 node
-enb2 = request.RawPC("DS")
+enb2 = request.RawPC("enb2")
 enb2.hardware_type = GLOBALS.HWTYPE
 enb2.disk_image = GLOBALS.SRSLTE_IMG
 iface4 = enb2.addInterface("eth1")
